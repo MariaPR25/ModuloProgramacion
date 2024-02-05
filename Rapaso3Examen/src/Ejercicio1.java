835d4c3b859b92c6acf0e671045aa4175e1b00dc
DAM1 <DAM1@pc02>
2024-02-05 09:58:13 +0100

    commit hecho

--- Rapaso3Examen/src/Ejercicio1.java
+++ Rapaso3Examen/src/Ejercicio1.java
@@ -6,7 +6,7 @@ private static final String [] DIAS_SEMANA ={"LUNES","MARTES","MIERCOLES","JUEVE
 	public static void main(String[] args) {
 		String dia;
 		boolean valido=false;
-		System.out.println("Introduce dia de la semana");
+		System.out.println("Introduce dia de la semana por favor");
 		dia=teclado.nextLine().toUpperCase();
 		for (int i = 0; i < DIAS_SEMANA.length; i++) {
 			if (DIAS_SEMANA[i].equals(dia)) {
